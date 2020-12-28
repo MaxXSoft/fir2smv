e5d7b44844dadf9d5e728c4349902e69743db103
MaxXSoft <x@MaxXSoft.net>
2020-12-28 18:49:51 +0800

    (Type) added method 'width'

--- src/main/scala/smv/Type.scala
+++ src/main/scala/smv/Type.scala
@@ -1,20 +1,24 @@
 package smv
 
 sealed trait Type {
+  def width: BigInt
   def serialize: String
 }
 
 // boolean type
 case object Boolean extends Type {
+  override def width: BigInt = 1
   override def serialize: String = "boolean"
 }
 
 // unsigned word[N] type
 case class UnsignedWord(n: BigInt) extends Type {
+  override def width: BigInt = n
   override def serialize: String = s"unsigned word[${n}]"
 }
 
 // signed word[N] type
 case class SignedWord(n: BigInt) extends Type {
+  override def width: BigInt = n
   override def serialize: String = s"signed word[${n}]"
 }
