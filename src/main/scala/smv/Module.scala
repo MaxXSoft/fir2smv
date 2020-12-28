c22f6360350268e317baf13999077e1cd07f53fc
MaxXSoft <x@MaxXSoft.net>
2020-12-28 19:52:12 +0800

    (Module) updated some details

--- src/main/scala/smv/Module.scala
+++ src/main/scala/smv/Module.scala
@@ -2,21 +2,19 @@ package smv
 
 import scala.collection.mutable.HashMap
 
+// representing a SMV module
 class Module(val name: String) {
   // clock of current module
   private var clock: Option[String] = None
 
   // all port variable declarations
-  private val ports = HashMap[String, Type]()
+  private val ports = HashMap[String, Port]()
 
   // all register variable declarations
-  private val regs = HashMap[String, Type]()
+  private val regs = HashMap[String, Register]()
 
   // all wire variable declarations
-  private val wires = HashMap[String, Type]()
-
-  // all variable assignments
-  private val assigns = HashMap[String, String]()
+  private val wires = HashMap[String, Wire]()
 
   // check clock in current module
   def checkClock(name: String): Unit = clock match {
@@ -26,46 +24,53 @@ class Module(val name: String) {
 
   // add a port declaration
   def addPort(name: String, portType: Type): Unit = {
-    ports += (name -> portType)
+    ports += (name -> Port(portType, name))
   }
 
+  // get the specific port declaration
+  def getPort(name: String): Option[Port] = ports.get(name)
+
   // add a register declaration
-  def addReg(name: String, regType: Type): Unit = {
-    regs += (name -> regType)
+  def addReg(name: String, regType: Type, reset: IR, init: IR): Unit = {
+    regs += (name -> Register(regType, name, reset, init))
   }
+
+  // get the specific register declaration
+  def getReg(name: String): Option[Register] = regs.get(name)
 
   // add a wire declaration
   def addWire(name: String, wireType: Type): Unit = {
-    wires += (name -> wireType)
+    wires += (name -> Wire(wireType, name))
   }
 
-  // add a assignment
-  def addAssign(name: String, expr: String): Unit = {
-    assigns += (name -> expr)
-  }
+  // get the specific wire declaration
+  def getWire(name: String): Option[Wire] = wires.get(name)
 
   // serialize
   def serialize: String = {
-    def flatten[T](m: HashMap[String, T]): String = {
-      m.map {
-        case (s, t: Type) => s"${s}: ${t.serialize}"
-        case (s, t) => s"${s}: ${t}"
-      }.mkString(";\n    ")
+    def flattenDeclaration[T <: Variable](m: HashMap[String, T]): String =
+      m.map { case (_, ir) => ir.declaration }.mkString("\n    ")
+
+    def flattenAssign[_ <: Variable](ms: HashMap[String, _]*): String = {
+      ms.filter { !_.isEmpty }.flatMap {
+        _.map { case (_, ir) => ir.asInstanceOf[Variable].assignment }
+         .filter { _ != None }
+      }.mkString("\n    ")
     }
 
     s"""MODULE ${name}
        |  VAR
        |    -- Ports
-       |    ${flatten(ports)}
+       |    ${flattenDeclaration(ports)}
        |
        |    -- Registers
-       |    ${flatten(regs)}
+       |    ${flattenDeclaration(regs)}
        |
        |    -- Wires
-       |    ${flatten(wires)}
+       |    ${flattenDeclaration(wires)}
        |
        |  ASSIGN
-       |    ${flatten(assigns)}
+       |    ${flattenAssign(ports, regs, wires)}
      """.stripMargin
   }
 }
