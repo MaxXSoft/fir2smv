7b8f763f3718f4741473ecb0eca0b636e4a44f14
MaxXSoft <x@MaxXSoft.net>
2020-12-29 21:06:44 +0800

    (SmvEmitter) added more details

--- src/main/scala/emitter/SmvEmitter.scala
+++ src/main/scala/emitter/SmvEmitter.scala
@@ -3,7 +3,9 @@ package emitter
 import firrtl._
 import firrtl.ir._
 
-import smv.{File, Module, Boolean, UnsignedWord, SignedWord}
+import smv.{File, Module}
+import smv.{Boolean, UnsignedWord, SignedWord}
+import smv.{IR, Variable, Op}
 
 // emit SMV code for model checking
 object SmvEmitter {
@@ -13,6 +15,7 @@ object SmvEmitter {
     file
   }
 
+  // callback for traversing modules
   def walkModule(f: File)(m: DefModule): DefModule = {
     val module = new Module(m.name)
     // traverse all ports
@@ -24,6 +27,7 @@ object SmvEmitter {
     m
   }
 
+  // callback for traversing ports
   def walkPort(m: Module)(p: Port): Port = {
     p.tpe match {
       case ClockType => m.checkClock(p.name)
@@ -32,34 +36,77 @@ object SmvEmitter {
     p
   }
 
+  // callback for traversing statements
   def walkStatement(m: Module)(s: Statement): Statement = {
     s match {
       case Block(_) => s.mapStmt(walkStatement(m))
-      case DefRegister(info, name, tpe, _, reset, init) => {
-        // TODO: use info
+      case Connect(_, Reference(name, _, _, _), expr) => {
+        m.getVariable(name).get.connect = Some(toSmvIR(expr))
       }
-      case DefNode(info, name, value) => {
-        //
-      }
-      case Connect(info, loc, expr) => {
-        //
+      case DefNode(_, name, value) => m.addWire(name, toSmvIR(value))
+      case DefRegister(_, name, tpe, _, reset, init) => {
+        m.addReg(name, toSmvType(tpe), toSmvIR(reset), toSmvIR(init))
       }
     }
     s
   }
 
-  def walkExpression(e: Expression): Expression = {
-    val visited = e.mapExpr(walkExpression)
-    println(visited.tpe)
-    visited
+  // convert Firrtl type to SMV type
+  def toSmvType(t: firrtl.ir.Type): smv.Type = t match {
+    case UIntType(IntWidth(n)) => if (n == 1) { Boolean }
+                                  else { UnsignedWord(n) }
+    case SIntType(IntWidth(n)) => SignedWord(n)
   }
 
-  def toSmvType(t: firrtl.ir.Type): smv.Type = t match {
-    case UIntType(IntWidth(n)) => if (n == 1) {
-      Boolean
-    } else {
-      UnsignedWord(n)
+  // convert 'PrimOp' to SMV 'Op'
+  // returns (Smv.Op, IsBinOp)
+  def toSmvOp(op: PrimOp): (Op, Boolean) = op match {
+    case PrimOps.Not => (smv.Not, false)
+    case PrimOps.And => (smv.And, true)
+    case PrimOps.Or => (smv.Or, true)
+    case PrimOps.Xor => (smv.Xor, true)
+    case PrimOps.Eq => (smv.Equal, true)
+    case PrimOps.Neq => (smv.NotEqual, true)
+    case PrimOps.Lt => (smv.Lt, true)
+    case PrimOps.Gt => (smv.Gt, true)
+    case PrimOps.Leq => (smv.Le, true)
+    case PrimOps.Geq => (smv.Ge, true)
+    case PrimOps.Neg => (smv.Neg, false)
+    case PrimOps.Add => (smv.Add, true)
+    case PrimOps.Sub => (smv.Sub, true)
+    case PrimOps.Mul => (smv.Mul, true)
+    case PrimOps.Div => (smv.Div, true)
+    case PrimOps.Rem => (smv.Mod, true)
+    case PrimOps.Shr | PrimOps.Dshr => (smv.Shr, true)
+    case PrimOps.Shl | PrimOps.Dshl => (smv.Shl, true)
+    case PrimOps.Cat => (smv.Cat, true)
+    case PrimOps.Tail => (smv.Nop, false)
+    case _ => ???
+  }
+
+  // convert expression to SMV IR
+  def toSmvIR(e: Expression): IR = e match {
+    case DoPrim(op, args, _, tpe) => toSmvOp(op) match {
+      case (sop, true) => smv.BinaryExpr(toSmvType(tpe), sop,
+                                         toSmvIR(args(0)),
+                                         toSmvIR(args(1)))
+      case (sop, false) => smv.UnaryExpr(toSmvType(tpe), sop,
+                                         toSmvIR(args(0)))
     }
-    case SIntType(IntWidth(n)) => SignedWord(n)
+    case Mux(cond, tval, fval, tpe) => smv.Mux(toSmvType(tpe),
+                                               toSmvIR(cond),
+                                               toSmvIR(tval),
+                                               toSmvIR(fval))
+    case Reference(name, tpe, _, _) => smv.Ref(toSmvType(tpe), name)
+    case SIntLiteral(value, IntWidth(w)) => smv.WordLiteral(SignedWord(w),
+                                                            value)
+    case UIntLiteral(value, IntWidth(w)) => {
+      if (w == 1) {
+        smv.BooleanLiteral(Boolean, value != 0)
+      } else {
+        smv.WordLiteral(UnsignedWord(w), value)
+      }
+    }
+    case _ => ???
   }
 }
