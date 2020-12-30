c93204c72403d1316ff521ccf44a94e850667727
MaxXSoft <x@MaxXSoft.net>
2020-12-30 10:19:08 +0800

    (FirrtlEmitter) fixed some faults about module type

--- src/main/scala/emitter/FirrtlEmitter.scala
+++ src/main/scala/emitter/FirrtlEmitter.scala
@@ -3,19 +3,19 @@ package emitter
 import chisel3._
 import chisel3.stage.ChiselStage
 
-import firrtl.{CircuitState, Parser, LowFirrtlEmitter}
+import firrtl.{CircuitState, Parser, VerilogEmitter}
 import firrtl.ir.Circuit
 import firrtl.options.Dependency
 import firrtl.stage.transforms.Compiler
 
 object FirrtlEmitter {
-  def apply[T <: Module](gen: () => T): Circuit = {
+  def apply[T <: RawModule](gen: () => T): Circuit = {
     // get chirrtl input
     val input = (new ChiselStage).emitChirrtl(gen())
     // parse the input
     val state = CircuitState(Parser.parse(input), Seq())
     // compile to low-firrtl
-    val compiler = new Compiler(Seq(Dependency[LowFirrtlEmitter]))
+    val compiler = new Compiler(Seq(Dependency[VerilogEmitter]))
     val finalState = compiler.execute(state)
     // emit output
     finalState.circuit
