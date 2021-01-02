81489ccd99716debd47c9a715932af0fb2ffda2a
MaxXSoft <x@MaxXSoft.net>
2021-01-02 13:09:54 +0800

    (Module) updated some details about memory

--- src/main/scala/smv/Module.scala
+++ src/main/scala/smv/Module.scala
@@ -3,7 +3,7 @@ package smv
 import scala.collection.mutable.{ListBuffer, HashMap}
 
 // representing a SMV module
-class Module(val name: String) {
+class Module(val file: File, val name: String) {
   // clock of current module
   private var clock: Option[String] = None
 
@@ -16,8 +16,14 @@ class Module(val name: String) {
   // all wire variable declarations
   private val wires = ListBuffer[Wire]()
 
+  // all memory port variable declarations
+  private val memPorts = ListBuffer[MemoryPort]()
+
   // all declared variables
   private val vars = HashMap[String, Variable]()
+
+  // all memory declarations (name, type, awidth)
+  private val mems = ListBuffer[(String, Memory, Int)]()
 
   // check clock in current module
   def checkClock(name: String): Unit = clock match {
@@ -47,17 +53,43 @@ class Module(val name: String) {
     vars += (name -> wire)
   }
 
+  // add a memory port declaration
+  def addMemPort(name: String, value: IR): Unit = {
+    val memPort = MemoryPort(value.irType, name)
+    memPort.connect = Some(value)
+    memPorts += memPort
+    vars += (name -> memPort)
+  }
+
+  // add a memory declaration
+  def addMemory(name: String, dataType: Type, depth: BigInt, wlat: Int,
+                rlat: Int, reader: String, writer: String): Unit = {
+    val addrWidth = chisel3.util.log2Ceil(depth)
+    val mem = MemoryModule(dataType, wlat, rlat, reader, writer)
+    file.addMemory(Reader(dataType, rlat))
+    file.addMemory(Writer(dataType, wlat))
+    file.addMemory(mem)
+    val a = (name, mem, addrWidth)
+    mems += a
+  }
+
   // get the specific variable
   def getVariable(name: String): Option[Variable] = vars.get(name)
 
   // serialize
   def serialize: String = {
-    def flattenDeclaration[T <: Variable](m: ListBuffer[T]): String =
-      m.map { case ir => ir.declaration }.mkString("\n    ")
+    def flattenVarDecl[T <: Variable](l: ListBuffer[T]): String =
+      l.map { _.declaration }.mkString("\n    ")
 
-    def flattenAssign[_ <: Variable](ms: ListBuffer[_]*): String = {
-      ms.filter { !_.isEmpty }.flatMap {
-        _.map { case ir => ir.asInstanceOf[Variable].assignment }
+    def flattenMemDecl(l: ListBuffer[(String, Memory, Int)]) = {
+      l.map {
+        case (name, mem, awidth) => s"$name: ${mem.typeName}($awidth);"
+      }.mkString("\n    ")
+    }
+
+    def flattenAssign[_ <: Variable](ls: ListBuffer[_]*): String = {
+      ls.filter { !_.isEmpty }.flatMap {
+        _.map { _.asInstanceOf[Variable].assignment }
          .filter { _ != None }.map { _.get }
       }.mkString("\n    ")
     }
@@ -65,16 +97,19 @@ class Module(val name: String) {
     s"""MODULE $name
        |  VAR
        |    -- Ports
-       |    ${flattenDeclaration(ports)}
+       |    ${flattenVarDecl(ports)}
        |
        |    -- Registers
-       |    ${flattenDeclaration(regs)}
+       |    ${flattenVarDecl(regs)}
        |
        |    -- Wires
-       |    ${flattenDeclaration(wires)}
+       |    ${flattenVarDecl(wires)}
+       |
+       |    -- Memories
+       |    ${flattenMemDecl(mems)}
        |
        |  ASSIGN
-       |    ${flattenAssign(ports, regs, wires)}
+       |    ${flattenAssign(ports, regs, wires, memPorts)}
      """.stripMargin
   }
 }
